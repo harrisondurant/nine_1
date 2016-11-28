b4e5e71c1053f8a31276d52b06bf4c7619e841c4
Elon University <hdurant2@mooney202-23.elon.edu>
2016-11-28 14:36:30 -0500

    Initial Commit

---
+++ nine_1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  nine_1
+//
+//  Created by Harrison Durant on 11/28/16.
+//  Copyright © 2016 Harrison Durant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
